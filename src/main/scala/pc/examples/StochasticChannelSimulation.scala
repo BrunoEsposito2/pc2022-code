5980570edfe154b8f0d5726dfc4d3cddfc24b607
BrunoEsposito2 <bruno.esposito2@studio.unibo.it>
2022-12-07 10:05:07 +0100

    add exercises setting

--- src/main/scala/pc/examples/StochasticChannelSimulation.scala
+++ src/main/scala/pc/examples/StochasticChannelSimulation.scala
@@ -1,8 +1,26 @@
 package pc.examples
 
-import pc.utils.Time
 import java.util.Random
 import pc.examples.StochasticChannel.*
+import pc.utils.Time
+
+/***
+ * LAB 3 - 16/11/22
+ *
+ * API for "The simulator" task
+ *
+ * @param simulation is the StochasticChannel
+ * @param runs
+ * @tparam A
+ */
+class StochasticChannelSimulation[A](simulation: A, runs: Int):
+  val t0: Long = ??? //java.lang.System.nanoTime
+  def avgRunTime: Double = ???
+  def failPercentage: Double = ???
+  private def runSimulation(v: =>A): Unit = ???
+
+object StochasticChannelSimulation
+
 
 @main def mainStochasticChannelSimulation =
   Time.timed(
@@ -10,3 +28,5 @@ import pc.examples.StochasticChannel.*
                            .take(10)
                            .toList
                            .mkString("\n")))
+
+  StochasticChannelSimulation(stocChannel, 10)
